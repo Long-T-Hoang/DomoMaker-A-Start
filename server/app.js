e5c998028306d945d62a6744792940f6a7f85f96
Long Hoang <heroprotunlong@gmail.com>
2021-11-04 15:15:04 -0400

    added login and signup
    - render corresponding pages
    - add login and signup function

---
+++ server/app.js
@@ -0,0 +1,51 @@
+const path = require('path');
+const express = require('express');
+const compression = require('compression');
+const favicon = require('serve-favicon');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const expressHandlebars = require('express-handlebars');
+
+const port = process.env.PORT || process.env.NODE_PORT || 3000;
+
+// use this when connected to Heroku
+//const dbURL = process.env.MONGODB_URI || 'mongodb://localhost/DomoMaker';
+
+// otherwise use this
+// delete before upload to GitHub
+const dbURL = 'mongodb+srv://lth1092:Vrael13071527@cluster0.ywnt7.mongodb.net/DomoMaker?retryWrites=true&w=majority';
+
+// attempt connection to MongoDB
+mongoose.connect(dbURL, (err) => {
+    if(err) {
+        console.log('Could not connect to database');
+        throw err;
+    }
+});
+
+const router = require('./router.js');
+
+// setting up express
+const app = express();
+
+app.use('/assets', express.static(path.resolve(`${__dirname}/../hosted/`)));
+app.use(favicon(`${__dirname}/../hosted/img/favicon.png`));
+app.use(compression());
+app.use(bodyParser.urlencoded({
+    extended: true,
+}));
+app.engine('handlebars', expressHandlebars({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+app.set('views', `${__dirname}/../views`);
+app.use(cookieParser());
+
+router(app);
+
+// attempt to start page
+app.listen(port, (err) => {
+    if(err) {
+        throw err;
+    }
+    console.log(`Listening on port ${port}`);
+});
