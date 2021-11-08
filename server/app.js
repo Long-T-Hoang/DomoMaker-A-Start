d573c3a1f7d9bd3392583338b1aff61acadd562d
Long Hoang <heroprotunlong@gmail.com>
2021-11-08 16:54:04 -0500

    DomoMaker C

--- server/app.js
+++ server/app.js
@@ -7,15 +7,19 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const expressHandlebars = require('express-handlebars');
 const session = require('express-session');
+const RedisStore = require('connect-redis')(session);
+const url = require('url');
+const redis = require('redis');
+const csrf = require('csurf');
 
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
 // use this when connected to Heroku
-// const dbURL = process.env.MONGODB_URI || 'mongodb://localhost/DomoMaker';
+const dbURL = process.env.MONGODB_URI || 'mongodb://localhost/DomoMaker';
 
 // otherwise use this
 // delete before upload to GitHub
-const dbURL = 'mongodb+srv://lth1092:Vrael13071527@cluster0.ywnt7.mongodb.net/DomoMaker?retryWrites=true&w=majority';
+//const dbURL = 'mongodb+srv://lth1092:Vrael13071527@cluster0.ywnt7.mongodb.net/DomoMaker?retryWrites=true&w=majority';
 
 // attempt connection to MongoDB
 mongoose.connect(dbURL, (err) => {
@@ -24,6 +28,25 @@ mongoose.connect(dbURL, (err) => {
     throw err;
   }
 });
+
+let redisURL = {
+  hostname: 'redis-10048.c291.ap-southeast-2-1.ec2.cloud.redislabs.com',
+  port: 10048,
+}
+
+let redisPASS = 'aA9myz8n9XvFijZCYLDgJS58oYKoekqJ';
+
+if(process.env.REDISCLOUD_URL)
+{
+  redisURL = url.parse(process.env.REDISCLOUD_URL);
+  [, redisPASS]= redisURL.auth.split(':');
+}
+
+let redisClient = redis.createClient({
+  host: redisURL.hostname,
+  port: redisURL.port,
+  password: redisPASS,
+})
 
 const router = require('./router.js');
 
@@ -38,14 +61,29 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(session({
   key: 'sessionid',
+  store: new RedisStore({
+    client: redisClient,
+  }),
   secret: 'Domo Arigato',
   resave: true,
   saveUninitialized: true,
+  cookie: {
+    httpOnly: true,
+  },
 }));
 app.engine('handlebars', expressHandlebars({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 app.set('views', `${__dirname}/../views`);
+app.disable('x-powered-by');
 app.use(cookieParser());
+
+app.use(csrf());
+app.use((err, req, res, next) => {
+  if(err.code !== 'EBADCSRFTOKEN') return next(err);
+
+  console.log('Missing CSRF token');
+  return false;
+});
 
 router(app);
 
